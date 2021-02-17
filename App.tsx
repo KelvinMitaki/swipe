31948677535c314ae95fe700c3d72a78c243afab
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-17 13:39:06 +0300

    added ball animation

--- App.tsx
+++ App.tsx
@@ -5,18 +5,11 @@ import Ball from "./components/Ball";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Ball />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+const styles = StyleSheet.create({});
--- components/Ball.tsx
+++ components/Ball.tsx
@@ -1,8 +1,33 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Animated, Button, StyleSheet, Text, View } from "react-native";
 
 const Ball = () => {
-  return <View style={styles.ball}></View>;
+  const [position] = useState<Animated.ValueXY>(
+    new Animated.ValueXY({ x: 0, y: 0 })
+  );
+  useEffect(() => {
+    Animated.spring(position, {
+      toValue: {
+        x: 200,
+        y: 500
+      },
+      useNativeDriver: false
+    }).reset();
+    Animated.spring(position, {
+      toValue: {
+        x: 200,
+        y: 500
+      },
+      useNativeDriver: false
+    }).start();
+  }, []);
+  return (
+    <View>
+      <Animated.View style={position.getLayout()}>
+        <View style={styles.ball}></View>
+      </Animated.View>
+    </View>
+  );
 };
 
 export default Ball;
@@ -11,6 +36,8 @@ const styles = StyleSheet.create({
   ball: {
     height: 60,
     width: 60,
-    borderRadius: 30
+    borderRadius: 5000,
+    borderColor: "black",
+    borderWidth: 30
   }
 });
